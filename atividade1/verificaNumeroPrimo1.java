18206bf4ae4fb63bafafd525b8b4eeb1eac964a2
kauamiguel <kauamcm@gmail.com>
2023-08-16 12:58:48 -0300

    Lista de exercicios atividade 01

---
+++ atividade1/verificaNumeroPrimo1.java
@@ -0,0 +1,31 @@
+
+package atividade1;
+
+import java.util.Scanner;
+
+public class verificaNumeroPrimo1{
+   
+   public static void main(String[] args) {
+      Scanner inputUser = new Scanner(System.in);
+      System.out.println("Digite um valor para verificar se \u00e9 primo: ");
+      int numberUser = inputUser.nextInt();
+      int isPrime = verificaNumeroPrimo(numberUser);
+      if (isPrime == 1) {
+         System.out.println("Numero primo");
+      } else {
+         System.out.println("Numero nao eh primo");
+      }
+      
+      inputUser.close();
+   }
+
+   static int verificaNumeroPrimo(int number) {
+   
+         for(int i = 2; i < number / 2; ++i) {
+            if (number % i == 0) {
+               return 0;
+            }
+         }
+         return 1;
+   }
+}
---
+++ testeAmbiente/Ambiente/teste.java
@@ -0,0 +1,7 @@
+package testeAmbiente.Ambiente;
+
+public class teste{
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
+}
